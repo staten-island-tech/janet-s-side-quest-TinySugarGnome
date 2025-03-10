78eec5101e5ee2810f1aad61eee7d85aea47457c
TinySugarGnome <nelsonouyang10@gmail.com>
2025-03-10 08:51:41 -0400

    skip row

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ print(data)  # Output the list
 
 
  """
-
+""" 
 numbers = [1, 2, 3, 4, 5]
 #make ur own function 
 def double(x):
@@ -29,3 +29,15 @@ doubled_numbers = map(double, numbers) #map = (function, what ur iterating)
 print(list(doubled_numbers))
     
 
+ """
+
+#skipping loops
+
+data = [
+    ["Store Name", "Day 1", "Day 2", "Day 3"],  # Header row (skip this)
+    ["Store A", 5000, 7000, 6500],
+    ["Store B", 8000, 6000, 7500]
+]
+
+for row in data[1:2:]:  # Skip the first AND row 
+    print(row)
